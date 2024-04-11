505b33857db3eaac1dd9e1e4568e255a8bd05fe3
RusticusMax <rusticusmax@gmail.com>
2024-04-10 22:06:28 -0400

    Removed manual and auto modes and related code, as well as dead code. Made go command to start moving

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
-#include <AccelStepper.h>
 
-// Define some steppers and the pins the will use
+// Define steppers and the pins the will use
 #define RIGHT_STEP_PIN 2
 #define RIGHT_DIR_PIN 3
 #define RIGHT_ENABLE_PIN 4
@@ -10,6 +9,7 @@
 #define LEFT_DIR_PIN 6
 #define LEFT_ENABLE_PIN 7
 
+// Make logical names for states
 #define LeftFWD LOW
 #define LeftREV HIGH
 #define RightFWD HIGH
@@ -17,21 +17,11 @@
 #define STEP_ENABLE LOW
 #define STEP_DISABLE HIGH
 
-#define AUTO_LOOP_CNT 70000
-#define AUTO_LOOP_DELAY 1
 #define MANUAL_LOOP_CNT 7000
 #define MANUAL_LOOP_DELAY 200
 
-#define RUN_MANUAL 1
-#define RUN_AUTO 2
-#define RUN_SERIAL 3
-int run_mode = RUN_SERIAL;
-
-AccelStepper leftTread;
-AccelStepper rightTread;
-unsigned long timeTillStep = 0; // time to run the motors
-unsigned long Speed = 10; // Speed in mm a second... someday ;) 
-unsigned long StepDelay = 0; // Delay between steps
+float Speed = 50.0; // Speed in mm a second
+unsigned long StepDelayMs = 0; // Delay between steps
 unsigned long LastStepTime = 0; // time of last step
 float DistanceMM = 10; // distance to run
 unsigned long Steps = 0; // steps to run
@@ -39,35 +29,28 @@ String Playback[] = {
   "s50 m80 r", 
   "s1000 m500 b",
   };
-int PlaybackIndex = -1;   // -1 is not playing back
-int PlaybackStep = 0;
-float PivotAngle = 0;
+int PlaybackIndex = -1;   // The predefined string to playback.  -1 is not playing back
+int PlaybackStep = 0;     // The current step in the playback string
+float PivotAngle = 0; // Angle to pivot with differential drive
 
-unsigned int i = 0;
-bool executeMove = false;
+// unsigned int i = 0;
+bool executeMove = false; // Flag to process the timeToStep function to run the motors
 
 // put function declarations here:
 void manualRun(unsigned int step_cnt, unsigned int delay);
 void manualSetup();
 void manualTestDrive();
 
-void autoRun(long step_cnt);
-void autoSetup();
-void autoSetSpeed(float speed);
-void autoSetSpeedR(float speed);
-void autoSetSpeedL(float speed);
-void autoTestDrive();
-void initializeAccelLib();
 unsigned long mmToSteps(float mm);
-unsigned long mmToDelay(float mm);
+unsigned long mmASecToMsDelay(float mm);
 int setDistanceMm(float mm);
 void stepIfTime(unsigned long speed);
 void stepMotors();
-void byHand();
 float getParam();
 char scanChar();
 float degToDiffDistance(float deg);
 void prepToMove();
+void stopAllMotors();
 
 // Main Code
 void setup() {
@@ -75,160 +58,141 @@ void setup() {
   Serial.begin(115200);
   Serial.print("Starting up\n");
 
-  switch (run_mode) {
-    case RUN_MANUAL:
-      manualSetup();
-      break;
-    case RUN_AUTO:
-      initializeAccelLib();
-      autoSetup();
-      break;
-    case RUN_SERIAL:
-      manualSetup();
-      Serial.print("Seral mode\n");
-      Serial.print("Enter command:\n");
-      Serial.print("f = Forward\n");
-      Serial.print("b = Back\n");
-      Serial.print("r = Right\n");
-      Serial.print("l = Left\n");
-      Serial.print("e = Engage Motors\n");
-      Serial.print("d = Disengage Motors\n");
-      Serial.print("sxx = Set Speed\n");
-      Serial.print("mxx = Set Distance in mm\n");
-      Serial.print("axx = Set Absolute Steps directly\n");
-      Serial.print("t = Test\n");
-      break;
-  }
+  manualSetup();
+  Serial.print("Seral mode\n");
+  Serial.print("Enter command:\n");
+  Serial.print("f = Set Forward\n");
+  Serial.print("b = Set Backward\n");
+  Serial.print("r = Set Right\n");
+  Serial.print("l = Set Left\n");
+  Serial.print("px.x = Set pivot in degrees\n");
+  Serial.print("g = Go based in set speed/distance/direction\n");
+  Serial.print("e = Engage Motors\n");
+  Serial.print("d = Disengage Motors\n");
+  Serial.print("sx.x = Set Speed in mm/sec\n");
+  Serial.print("mx.x = Set Distance in mm\n");
+  Serial.print("axx = Set Absolute Steps directly\n");
+  Serial.print("Px = Playback preset command sequence\n");
 }
 
 void loop() {
   char inChar = 0;
-  //digitalWrite(LED_BUILTIN, HIGH);  // Singnle start
 
-  switch (run_mode) {
-    case RUN_MANUAL:
-      manualTestDrive();
-      break;
-    case RUN_AUTO:
-      autoTestDrive();
-      break;
-    case RUN_SERIAL:
-      while (true) {
-        if((inChar = scanChar()) != 0) { // If there is a character in the buffer, read it and act on it
-          Serial.print("Received: (");
-          Serial.print(inChar, HEX);
+  if((inChar = scanChar()) != 0) { // If there is a character in the buffer, read it and act on it
+    Serial.print("Received: (");
+    Serial.print(inChar, HEX);
+    Serial.print(")\n");
+    switch (inChar) {  //fbrldesmaPp  abdeflmprsP
+      case 'f':
+        Serial.print("Forward\n");
+        digitalWrite(LEFT_DIR_PIN, LeftFWD);
+        digitalWrite(RIGHT_DIR_PIN, RightFWD);
+        break;
+      case 'b':
+        Serial.print("Backward\n");
+        digitalWrite(LEFT_DIR_PIN, LeftREV);
+        digitalWrite(RIGHT_DIR_PIN, RightREV);
+        break;
+      case 'r':
+        Serial.print("Right\n");
+        digitalWrite(LEFT_DIR_PIN, LeftFWD);
+        digitalWrite(RIGHT_DIR_PIN, RightREV);
+        break;
+      case 'l':
+        Serial.print("Left\n");
+        digitalWrite(LEFT_DIR_PIN, LeftREV);
+        digitalWrite(RIGHT_DIR_PIN, RightFWD);
+        break;
+      case 'p': // Pivot right by n degrees
+        PivotAngle = getParam();
+        DistanceMM = degToDiffDistance(PivotAngle);
+        Steps = setDistanceMm(DistanceMM);
+        digitalWrite(LEFT_DIR_PIN, LeftFWD);
+        digitalWrite(RIGHT_DIR_PIN, RightREV);
+        Serial.print("Pivot Set: (");
+        Serial.print(PivotAngle);
+        Serial.print(")\n");
+        break;
+      case 'g':
+        Serial.print("Go\n");
+        prepToMove();
+        break;
+      case 'h':
+        Serial.print("Halt\n");
+        stopAllMotors();
+        break;
+      case 'd':
+        Serial.print("DisableMotors\n");
+        digitalWrite(LEFT_ENABLE_PIN, STEP_DISABLE);
+        digitalWrite(RIGHT_ENABLE_PIN, STEP_DISABLE);
+        break;
+      case 'e':
+        Serial.print("Enable Motors\n");
+        digitalWrite(LEFT_ENABLE_PIN, STEP_ENABLE);
+        digitalWrite(RIGHT_ENABLE_PIN, STEP_ENABLE);
+        break;
+      case 's':
+        Serial.print("Set Speed\n");
+        Speed = getParam();
+        StepDelayMs = mmASecToMsDelay(Speed);
+        Serial.print("Speed Set: (");
+        Serial.print(Speed);
+        Serial.print(")\n");
+        break;
+      case 'm':
+        Serial.print("Set Distance\n");
+        DistanceMM = getParam();
+        Steps = setDistanceMm(DistanceMM);
+        Serial.print("Distance Set: (");
+        Serial.print(DistanceMM);
+        Serial.print(")\n");
+        break;
+      case 'a':
+        Serial.print("Set Absolute Steps\n");
+        Steps = getParam();
+        Serial.print("Steps Set: (");
+        Serial.print(Steps);
+        Serial.print(")\n");
+        break;
+      case 'P':
+        //if((inChar = scanChar()) != 0) { // see which playback to run
+        PlaybackIndex = (int)getParam();
+        Serial.print("Received: (");
+        Serial.print(inChar, HEX);
+        Serial.print(")\n");
+        if(PlaybackIndex >= 0 && PlaybackIndex < (int)sizeof(Playback)) {
+          Serial.print("Playback: (");
+          Serial.print(Playback[PlaybackIndex]);
           Serial.print(")\n");
-          switch (inChar) {  //fbrldesmtaPp  abdeflmprstP
-            case 'f':
-              Serial.print("Forward\n");
-              digitalWrite(LEFT_DIR_PIN, LeftFWD);
-              digitalWrite(RIGHT_DIR_PIN, RightFWD);
-              prepToMove();
-              break;
-            case 'b':
-              Serial.print("Backward\n");
-              digitalWrite(LEFT_DIR_PIN, LeftREV);
-              digitalWrite(RIGHT_DIR_PIN, RightREV);
-              prepToMove();
-              break;
-            case 'r':
-              Serial.print("Right\n");
-              digitalWrite(LEFT_DIR_PIN, LeftFWD);
-              digitalWrite(RIGHT_DIR_PIN, RightREV);
-              prepToMove();
-              break;
-            case 'l':
-              Serial.print("Left\n");
-              digitalWrite(LEFT_DIR_PIN, LeftREV);
-              digitalWrite(RIGHT_DIR_PIN, RightFWD);
-              prepToMove();
-              break;
-            case 'd':
-              Serial.print("DisableMotors\n");
-              digitalWrite(LEFT_ENABLE_PIN, STEP_DISABLE);
-              digitalWrite(RIGHT_ENABLE_PIN, STEP_DISABLE);
-              break;
-            case 'e':
-              Serial.print("Enable Motors\n");
-              digitalWrite(LEFT_ENABLE_PIN, STEP_ENABLE);
-              digitalWrite(RIGHT_ENABLE_PIN, STEP_ENABLE);
-              break;
-            case 's':
-              Serial.print("Set Speed\n");
-              Speed = getParam();
-              StepDelay = mmToDelay(Speed);
-              Serial.print("Speed Set: (");
-              Serial.print(Speed);
-              Serial.print(")\n");
-              break;
-            case 'm':
-              Serial.print("Set Distance\n");
-              DistanceMM = getParam();
-              Steps = setDistanceMm(DistanceMM);
-              Serial.print("Distance Set: (");
-              Serial.print(DistanceMM);
-              Serial.print(")\n");
-              break;
-            case 'a':
-              Serial.print("Set Absolute Steps\n");
-              Steps = getParam();
-              Serial.print("Steps Set: (");
-              Serial.print(Steps);
-              Serial.print(")\n");
-              break;
-            case 't':
-              Serial.print("Test Routine\n");
-              Serial.print("drive by hand\n");
-              digitalWrite(LEFT_DIR_PIN, LeftFWD);
-              digitalWrite(RIGHT_DIR_PIN, RightFWD);
-              byHand();
-            case 'P':
-              if((inChar = scanChar()) != 0) { // see witch playback to run
-                Serial.print("Received: (");
-                Serial.print(inChar, HEX);
-                Serial.print(")\n");
-                if((inChar = inChar - '0') >= 0 && (unsigned long) inChar < sizeof(Playback)) {
-                  Serial.print("Playback: (");
-                  Serial.print(Playback[(int)inChar]);
-                  Serial.print(")\n");
-                  PlaybackIndex = (int)inChar;  // Turn on playback mode
-                  PlaybackStep = 0;
-                } else {
-                  Serial.print("Invalid playback\n");
-                }
-              }
-              break;
-            case 'p': // Pivot right by n degrees
-              PivotAngle = getParam();
-              DistanceMM = degToDiffDistance(PivotAngle);
-              Steps = setDistanceMm(DistanceMM);
-              Speed = 50; // 50mm/s
-              digitalWrite(LEFT_DIR_PIN, LeftFWD);
-              digitalWrite(RIGHT_DIR_PIN, RightREV);
-              Serial.print("Pivot Set: (");
-              Serial.print(PivotAngle);
-              Serial.print(")\n");
-              executeMove = true;
-              break;
-            default:
-              Serial.print("Invalid command\n");
-              break;
-          }
+          PlaybackStep = 0;
+        } else {
+          Serial.print("Invalid playback\n");
+          PlaybackIndex = -1;
         }
-        stepIfTime(Speed);
-      }
-    default:
-      Serial.print("Invalid run mode?!?\n");
-      break;
+        //}
+        break;
+      case 'w':
+        Serial.print("Wait\n");
+        delay(getParam());
+        break;
+      default:
+        Serial.print("Invalid command\n");
+        break;
+    }
   }
-  digitalWrite(LED_BUILTIN, LOW);  // Signal end
-  exit(0);
+  stepIfTime(Speed);
 }
 
 void prepToMove() {
   Steps = setDistanceMm(DistanceMM);
-  StepDelay = mmToDelay(Speed);
+  StepDelayMs = mmASecToMsDelay(Speed);
   executeMove = true;
+}
+
+void stopAllMotors() {
+  // Steps = 0;
+  // StepDelayMs = 0;
+  executeMove = false;
 }
 
 // Scan input for char nonblocking 
@@ -246,14 +210,6 @@ char scanChar() {
     inChar = Serial.read();
   }
   return inChar;
-}
-
-void byHand() {
-  for(i=0; i < 1000; i++) {
-    Serial.print("by hand\n");
-    stepMotors();
-    delay(1);
-  }
 }
 
 // Convert degrees to distance in mm for differential drive
@@ -280,15 +236,14 @@ float getParam() {
 }
 
 //
-// Basic manual control functions
+// Basic control functions
 //
-
 void stepIfTime(unsigned long speed) {
   // unsigned long curMicros = micros(); // time to run the motors
 
-  if (executeMove && StepDelay > 0 && Steps > 0) { // If speed is 0, or steps are 0, No stepping
+  if (executeMove && StepDelayMs > 0 && Steps > 0) { // If speed is 0, or steps are 0, No stepping
     // Deal with wraparound of micros()
-    if (micros() - LastStepTime >= StepDelay) {
+    if (micros() - LastStepTime >= StepDelayMs) {
       stepMotors();
       LastStepTime = micros();
       Steps--;
@@ -314,7 +269,7 @@ unsigned long mmToSteps(float mm) {
 // 200 steps per rev, 1.8 degrees per step, 38mm per rev (wheel diameter)
 // ideally 5ms per step is 1 rev per second = 38mm per second
 // 38mm/200steps = 0.19mm per step
-unsigned long mmToDelay(float mm) { 
+unsigned long mmASecToMsDelay(float mm) { 
   float tmp;
   tmp = 1000000.0/(mm/0.04);  // Delay in microseconds
   Serial.print("Delay: (");
@@ -330,8 +285,6 @@ void stepMotors(){
   digitalWrite(LEFT_STEP_PIN, LOW);
   digitalWrite(RIGHT_STEP_PIN, LOW);
 }
-
-
 
 //
 // Manual control functions
@@ -358,22 +311,6 @@ void manualTestDrive()  {
   digitalWrite(RIGHT_DIR_PIN, RightFWD);
   manualRun(MANUAL_LOOP_CNT, MANUAL_LOOP_DELAY);
   delay(1000);
-
-//   // Spin in place to left
-//   digitalWrite(LEFT_DIR_PIN, LeftREV);
-//   digitalWrite(RIGHT_DIR_PIN, RightFWD);
-//   manualRun(MANUAL_LOOP_CNT, MANUAL_LOOP_DELAY);
-//   delay(1000);
-
-// // Spin in place to Right
-//   digitalWrite(LEFT_DIR_PIN, LeftFWD);
-//   digitalWrite(RIGHT_DIR_PIN, RightREV);
-//   manualRun(MANUAL_LOOP_CNT, MANUAL_LOOP_DELAY);
-//   delay(1000);
-
-//   // Disable motors... hopefully.
-//   digitalWrite(RIGHT_ENABLE_PIN, STEP_DISABLE);
-//   digitalWrite(LEFT_ENABLE_PIN, STEP_DISABLE);
 }
 
 // pulse pins high/low with delay between pulses to step motors
@@ -383,148 +320,3 @@ void manualRun(unsigned int step_cnt, unsigned int delay) {
     delayMicroseconds(delay);
   }
 }
-
-
-//
-// Auto control functions
-//
-void autoSetup()  {
-  leftTread.setEnablePin(RIGHT_ENABLE_PIN);
-  // Direction, step, enable
-  leftTread.setPinsInverted(true, false, true); // Invert enable (low = enabled)
-  leftTread.enableOutputs();
-  leftTread.setMinPulseWidth(20);
-
-  rightTread.setEnablePin(LEFT_ENABLE_PIN);
-  rightTread.setPinsInverted(false, false, true);
-  rightTread.enableOutputs();
-  rightTread.setMinPulseWidth(20);
-
-  leftTread.setMaxSpeed(10000.0);
-  leftTread.setAcceleration(500.0);
-  rightTread.setMaxSpeed(10000.0);
-  rightTread.setAcceleration(500.0);
-  Serial.print("Auto setup done\n");
-}
-
-void autoSetSpeed(float speed) {
-  leftTread.setSpeed(speed);
-  rightTread.setSpeed(speed);
-  Serial.print("Auto set speed done");
-}
-
-void autoSetSpeedR(float speed) {
-  rightTread.setSpeed(speed);
-  Serial.print("Auto set speed done");
-}
-
-void autoSetSpeedL(float speed) {
-  leftTread.setSpeed(speed);
-  Serial.print("Auto set speed done");
-}
-
-
-void autoTestDrive() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  
-  autoSetSpeed(3000.0);
-  autoRun(AUTO_LOOP_CNT);
-  delay(1000);
-
-  autoSetSpeedL(-3000.0);
-  autoRun(AUTO_LOOP_CNT);
-  delay(1000);
-
-  autoSetSpeedL(3000.0);
-  autoSetSpeedR(-3000.0);
-  autoRun(AUTO_LOOP_CNT);
-
-  digitalWrite(LED_BUILTIN, LOW);
-  leftTread.disableOutputs();
-  rightTread.disableOutputs();
-}
-
-void autoRun(long loop_cnt) {
-  long i = 0;
-  while(i < loop_cnt) {
-    i++;
-    leftTread.runSpeed();
-    rightTread.runSpeed();
-  }
-}
-
-void initializeAccelLib() {
-  leftTread = AccelStepper(AccelStepper::DRIVER, RIGHT_STEP_PIN, RIGHT_DIR_PIN);
-  rightTread = AccelStepper(AccelStepper::DRIVER, LEFT_STEP_PIN, LEFT_DIR_PIN);
-}
-//
-// End of auto control functions
-//
-
-
-// case 's':
-//           autoSetSpeed(-3000.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'a':
-//           autoSetSpeedL(-3000.0);
-//           autoSetSpeedR(3000.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'd':
-//           autoSetSpeedL(3000.0);
-//           autoSetSpeedR(-3000.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'q':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'e':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'z':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'x':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'r':
-//           autoSetSpeed(3000.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'f':
-//           autoSetSpeed(-3000.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 't':
-//           autoSetSpeedL(-3000.0);
-//           autoSetSpeedR(3000.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'g':
-//           autoSetSpeedL(3000.0);
-//           autoSetSpeedR(-3000.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'y':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'h':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'u':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'j':
-//           autoSetSpeed(0.0);
-//           autoRun(AUTO_LOOP_CNT);
-//           break;
-//         case 'i':
-//           autoSetSpeed(3000.0);
